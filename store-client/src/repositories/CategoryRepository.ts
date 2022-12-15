7e7c2dd32c169aeb2d043d675bd4c34fd7158b50
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2022-12-15 23:36:02 +0700

    แสดงข้อมูล category ทั้งหมด, แก้ไข title

--- store-client/src/repositories/CategoryRepository.ts
+++ store-client/src/repositories/CategoryRepository.ts
@@ -4,8 +4,8 @@ import { IRepository } from "./IRepository";
 export class CategoryRepository implements IRepository<Category> {
     async getAll(): Promise<Category[] | null> {
         return [
-            {id : 1,title : 'Fantasy'},
-            {id : 2,title : 'Suspense'}
+            {id : 1,title : 'Drama'},
+            {id : 2,title : 'Romantic'}
         ]
     }
 }
