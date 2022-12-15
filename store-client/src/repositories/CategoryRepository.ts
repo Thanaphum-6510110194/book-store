bffaf08bef1c13ab1a97562d26ad6dd421f33b8e
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2022-12-16 01:00:00 +0700

    แก้ไขไฟล์ category ให้เชื่อมต่อกับฝั่ง backend

--- store-client/src/repositories/CategoryRepository.ts
+++ store-client/src/repositories/CategoryRepository.ts
@@ -1,23 +1,34 @@
 import Category from "../models/Category";
 import { IRepository } from "./IRepository";
+import config from "../config";
+import axios from 'axios';
 
 export class CategoryRepository implements IRepository<Category> {
-    get(id: string | number): Promise<Category[] | null> {
-        throw new Error("Method not implemented.");
-    }
-    create(entity: Category): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    update(entity: Partial<Category>): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
-    delete(id: string | number): Promise<void> {
-        throw new Error("Method not implemented.");
-    }
+    urlPrefix = config.remoteRepositoryUrlPrefix
+
     async getAll(): Promise<Category[] | null> {
-        return [
-            {id : 1,title : 'Drama'},
-            {id : 2,title : 'Romantic'}
-        ]
+        const result = await axios.get<Category[]>(`${this.urlPrefix}/category`)
+        return result.data
     }
+    async get(id: string | number): Promise<Category | null> {
+        const result = await axios.get<Category>(`${this.urlPrefix}/category/${id}`)
+        return result.data
+    } 
+    
+    async create(entity: Partial<Category>): Promise<void> {
+        const result = await axios.post<Category>(`${this.urlPrefix}/category`,entity)
+        console.log(result.data)
+    }
+
+    async update(entity: Partial<Category>): Promise<void> {
+        const result = await axios.put<Category>(`${this.urlPrefix}/category/${entity.id}`,entity)
+        console.log(result.data)
+    }
+
+    async delete(id: string | number): Promise<void> {
+        const result = await axios.delete<Category>(`${this.urlPrefix}/category/${id}`)
+        console.log(result.data)
+    }
+
+    
 }
--- store-client/src/repositories/IRepository.ts
+++ store-client/src/repositories/IRepository.ts
@@ -1,6 +1,6 @@
 export interface IRepository<T> {
     getAll():Promise<T[] | null>
-    get(id: string|number): Promise<T[] | null>
+    get(id: string|number): Promise<T | null>
     create(entity: T): Promise<void>
     update(entity: Partial<T>): Promise<void>
     delete(id: string|number): Promise<void>
