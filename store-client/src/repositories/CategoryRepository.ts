d3571453bc780ec05358798fe921cbb0c98e3fb6
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2022-12-16 00:25:35 +0700

    เติม method Category Repo ทั้งหมด

--- store-client/src/repositories/CategoryRepository.ts
+++ store-client/src/repositories/CategoryRepository.ts
@@ -2,6 +2,18 @@ import Category from "../models/Category";
 import { IRepository } from "./IRepository";
 
 export class CategoryRepository implements IRepository<Category> {
+    get(id: string | number): Promise<Category[] | null> {
+        throw new Error("Method not implemented.");
+    }
+    create(entity: Category): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    update(entity: Partial<Category>): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
+    delete(id: string | number): Promise<void> {
+        throw new Error("Method not implemented.");
+    }
     async getAll(): Promise<Category[] | null> {
         return [
             {id : 1,title : 'Drama'},
