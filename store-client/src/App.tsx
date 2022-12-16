e81d62c41958c7b8684f1801b6d87f4116854807
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2022-12-16 17:24:33 +0700

    แก้ไขให้แสดงผลจาก BookDetail

--- store-client/src/App.tsx
+++ store-client/src/App.tsx
@@ -1,9 +1,13 @@
 import Repo from './repositories'
 import Category from './models/Category';
 import { useEffect, useState } from 'react'
+import Book from './models/Book';
+import BookDetail from './components/BookDetail';
+
 
 function App() {
   const [categoryList, setCategoryList] = useState<Category[]>([])
+  const [bookList, setBookList] = useState<Book[]>([])
 
   const fetchCategoryList = async () => {
     const result = await Repo.categories.getAll()
@@ -12,15 +16,30 @@ function App() {
     }
   }
 
+  const fetchBookList = async () => {
+    const result = await Repo.books.getAll()
+    if(result){
+      setBookList(result)
+    }
+  }
+
   useEffect(() => {
     fetchCategoryList()
+    fetchBookList()
   })
   
   return (
-  <div>
-    {categoryList.map(category => <p key={category.id}> ID : {category.id} Title : {category.title}</p>)}
-  </div>
+    <div>
+      <div>
+      {categoryList.map(category => <p key={category.id}> ID : {category.id} Title : {category.title}</p>)}
+      </div>
+      <hr />
+      <div>
+        {bookList.map(book => <div key={book.id}><BookDetail {...book}/> <hr /> </div> )}
+      </div>
+    </div>
   )
 }
+
 
 export default App;
