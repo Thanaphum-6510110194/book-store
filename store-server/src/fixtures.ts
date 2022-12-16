507e010d6a59c1768347a76510ec376b44e5b95e
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2022-12-16 17:06:47 +0700

    แก้ไขข้อมูล fixtures

--- store-server/src/fixtures.ts
+++ store-server/src/fixtures.ts
@@ -4,11 +4,18 @@ async function loadFixtures(clearData = false) {
     if(clearData){
         console.warn('clearing data')
         await db('category').del()
+        await db('book').del()
     }
     
     await db.batchInsert('category',[
         {id: 1,title : 'Drama'},
         {id: 2,title : 'Programming'}
+    ])
+    
+    await db.batchInsert('book',[
+        {id: 1,title: 'Titanic',price: 180,stock_amount: 10,category_id:1},
+        {id: 2,title: 'The green mile',price: 420,stock_amount: 6,category_id:1},
+        {id: 3,title: 'Day this day 365',price: 1300,stock_amount: 3,category_id:2}
     ])
 }
 
