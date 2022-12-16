ef4d1f286b89c986722c640c849e8d895037fc35
ธนภูมิ บูบ่าสอ <neymar90130@gmail.com>
2022-12-16 17:27:12 +0700

    แก้ไขให้เรียกข้อมูล Book จาก db

--- store-client/src/repositories/BookRepository.ts
+++ store-client/src/repositories/BookRepository.ts
@@ -1,17 +1,30 @@
 import Book from "../models/Book";
 import { IRepository } from "./IRepository";
+import axios from "axios";
+import config from "../config";
+
 
 export class BookRepository implements IRepository<Book> {
+    urlPrefix = config.remoteRepositoryUrlPrefix
+
     async getAll(): Promise<Book[] | null> {
-        return [
-            {
-                id: 1, title: 'Titanic', price:10, stockAmount: 10,
-                category: {id : 1,title: 'Drama'}
-            },
-            {
-                id: 1, title: 'The green mile', price:420, stockAmount: 6,
-                category: {id: 1,title: 'Drama'}
-            }
-        ]
+        const result = await axios.get<Book[]>(`${this.urlPrefix}/book`)
+        return result.data
+    }
+    async get(id: number|string): Promise<Book | null>{
+        const result = await axios.get<Book>(`${this.urlPrefix}/book/${id}`)
+        return result.data
+    }
+    async create(entity: Partial<Book>): Promise<void> {
+        const result = await axios.post<Book>(`${this.urlPrefix}/book`,entity)
+        console.log(result.data)
+    }
+    async update(entity: Partial<Book>): Promise<void> {
+        const result = await axios.put<Book>(`${this.urlPrefix}/book/${entity.id}`,entity)
+        console.log(result.data)
+    }
+    async delete(id: number | string): Promise<void> {
+        const result = await axios.delete<Book>(`${this.urlPrefix}/book/${id}`)
+        console.log(result.data)
     }
 }
